3a1777cabb0dfe88a78ad4fcffd7d23c7f0a4322
AnyaKhabarova <76184706+AnyaKhabarova@users.noreply.github.com>
2021-02-17 22:10:29 -0800

    PenColor_PenCost

---
+++ Classes_n_Structures_6.2/Classes_n_Structures_6.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Classes_n_Structures_6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    
+    class Pen
+    {
+        public string color;
+        public int cost;
+
+        public Pen()
+        {
+            color = "black";
+            cost = 100;
+        }
+
+        public Pen(string penColor,int penCost)
+        {
+            color = penColor;
+            cost = penCost;
+        }
+    }
+}
